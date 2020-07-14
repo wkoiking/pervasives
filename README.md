b1495fbc5e57a7b6f7b33ebfc02c259fecf37349
Nao Fujinaga <wanagida@gmail.com>
2020-07-14 15:24:24 +0900

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 ﻿Alt Prelude for introduction of Haskell to OCaml programmers avoiding type class.
+
+# はじめ方
+
+先ずインターネットに繋がっていることを確認。
+
+## stackのインストール
+
+以下のURLから`stack`をインストールする。
+
+<https://docs.haskellstack.org/en/stable/install_and_upgrade/#windows>
+
+## `Git for Windows`のインストール
+
+以下のURLから`Git for Windows`をインストールする。
+
+<https://git-for-windows.github.io/>
+
+## Haskellを開始する！
+
+Git bashから下記のコマンドを実行する。
+
+~~~
+$ cd ~
+$ git clone https://github.com/wkoiking/pervasives.git
+$ cd pervasives
+$ stack setup
+$ stack ghci
+~~~
--- app/Main.hs
+++ app/Main.hs
@@ -12,8 +12,11 @@ main2 :: IO ()
 main2 = do
     -- 標準入力
     str <- getLine
+    -- 標準出力
     putStrLn str
 
+
+-- let と where による変数束縛
 main3 :: IO ()
 main3 = do
     str <- getLine
@@ -23,32 +26,22 @@ main3 = do
  where f :: Int -> Int
        f n = n + n
 
+
 main4 :: IO ()
 main4 = do
     str <- getLine
     let num :: Int
         num = int_of_string str
+    -- ifによる条件分岐
     if 10 < num
         then do
             putStrLn "You input: Large number"
         else do
             putStrLn ("You input: " ++ string_of_int num)
 
+-- 繰り返し処理（再帰）
 main5 :: IO ()
 main5 = do
-    str <- getLine
-    let mNum :: Maybe Int
-        mNum = int_of_string_opt str
-    case mNum of
-        Just num -> do
-            putStrLn (string_of_int (f num))
-        Nothing  -> do
-            putStrLn "You need to input a number ... bye!"
- where f :: Int -> Int
-       f n = n + n
-
-main6 :: IO ()
-main6 = do
     str <- getLine
     if str == "exit"
         then do
@@ -65,6 +58,9 @@ myNumber = 1
 myList :: [Int]
 myList = [1,2,3,4,5]
 
+myList2 :: [Int]
+myList2 = 1 : 2 : 3 : 4 : []
+
 -- 浮動小数点
 myFloatNumber :: Float
 myFloatNumber = 2 / 3
@@ -77,10 +73,15 @@ myChar = 'A'
 myString :: String
 myString = "Hello World"
 
+-- タプル
+myTuple :: (Int, String)
+myTuple = (1, "Hello")
+
 -- 関数
 myFunction :: Int -> Int
 myFunction n = n * n * n
 
+-- 無名関数
 myFunction2 :: Int -> Int
 myFunction2 = (\ n -> n * n * n)
 
@@ -90,6 +91,49 @@ printNumber n = do
     putStrLn "Printing number ..."
     putStrLn (string_of_int n)
 
+-- ユーザー定義の型
+
+-- * 列挙型
+data Color = Red | Blue | Black | White
+
+-- * レコード型
+data Person = Person
+    { name :: String
+    , age :: Int
+    }
+
+-- -- * 型変数を含むかた
+-- data MyMaybe a
+--     = MyJust a
+--     | MyNothing
+--
+-- -- * 再帰的な型
+-- data MyList a
+--     = Cons a MyList
+--     | Nill
+
+-- パターンマッチによる分岐
+string_of_color :: Color -> String
+string_of_color col = case col of
+    Red -> "red"
+    Blue -> "blue"
+    Black -> "black"
+    White -> "white"
+
+main5 :: IO ()
+main5 = do
+    str <- getLine
+    let mNum :: Maybe Int
+        mNum = int_of_string_opt str
+    -- パターンマッチによる分岐
+    case mNum of
+        Just num -> do
+            putStrLn (string_of_int (f num))
+        Nothing  -> do
+            putStrLn "You need to input a number ... bye!"
+ where f :: Int -> Int
+       f n = n + n
+
 main7 :: IO ()
 main7 = do
     -- forループ
@@ -97,28 +141,28 @@ main7 = do
 
 main8 :: IO ()
 main8 = do
-    -- 無名関数を使ってforループ処理
+    -- 無名関数を使って、値を束縛してforループ処理
     forM_ [1,2,3,4,5] $ \ n -> do
         putStrLn "Printing number ..."
         putStrLn (string_of_int n)
 
 main9 :: IO ()
 main9 = do
-    -- 初期化
+    -- 参照の初期化
     vNum <- newIORef 0 :: IO (IORef Int)
 
-    -- 変数を読む
+    -- 参照の中身を読む
     n1 <- readIORef vNum
     putStrLn "Initial number ..."
     putStrLn (string_of_int n1)
 
-    -- 変数に値を代入する
+    -- 参照の中身を書き換える（値を代入する）
     writeIORef vNum 10
     n2 <- readIORef vNum
     putStrLn "Substituted number ..."
     putStrLn (string_of_int n2)
 
-    -- 変数を変更する
+    -- 参照の中身を変更する
     modifyIORef vNum increment
     n3 <- readIORef vNum
     putStrLn "Modified number ..."
